ae4d5d4deac196893c12f70c82b57ccbfb4cda3c
reqwer <rostyktymk1896@gmail.com>
2024-05-30 20:48:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -25,4 +25,4 @@ def proxy():
     return Response(generate(), content_type=stream.headers['content-type'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443)
+    app.run(host='0.0.0.0', port=8080)
